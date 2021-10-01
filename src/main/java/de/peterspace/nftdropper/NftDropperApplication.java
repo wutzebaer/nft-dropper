54938a0221bab3161e922a21f4a20c8a594b2427
peter <petergrossmann@email.de>
2021-10-01 09:30:09 +0200

    init

---
+++ src/main/java/de/peterspace/nftdropper/NftDropperApplication.java
@@ -0,0 +1,13 @@
+package de.peterspace.nftdropper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftDropperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftDropperApplication.class, args);
+	}
+
+}
