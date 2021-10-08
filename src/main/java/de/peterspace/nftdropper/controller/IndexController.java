32835a0e8460fd1fa6447fd9a834558887d5eaec
peter <petergrossmann@email.de>
2021-10-08 22:41:35 +0200

    more vars

--- src/main/java/de/peterspace/nftdropper/controller/IndexController.java
+++ src/main/java/de/peterspace/nftdropper/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import de.peterspace.nftdropper.component.NftMinter;
 import de.peterspace.nftdropper.component.NftSupplier;
 import lombok.RequiredArgsConstructor;
 
@@ -15,12 +16,18 @@ public class IndexController {
 	@Value("${token.price}")
 	private long tokenPrice;
 
+	@Value("${token.maxAmount}")
+	private long tokenMaxAmount;
+
 	private final NftSupplier nftSupplier;
+	private final NftMinter nftMinter;
 
 	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("tokenLeft", nftSupplier.tokensLeft());
 		model.addAttribute("tokenPrice", tokenPrice);
+		model.addAttribute("tokenMaxAmount", tokenMaxAmount);
+		model.addAttribute("paymentAddress", nftMinter.getPaymentAddress());
 		return "index";
 	}
 
